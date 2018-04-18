755d208c881df842e9d2f4baa07627af672e44eb
Chris Cao <chriscao@vt.edu>
2018-04-18 13:59:33 -0400

    Imported twitter credentials to main file

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,14 +1,27 @@
 #General imports for analysis
 import tweepy
-import twitter-auth
+import sys
 import pandas as pnds 
-import numpys as nump 
+# import numpys as nump
 
 #Imports for visualizations
 from IPython.display import display
 import matplotlib.pyplot as plot
 import seaborn as sbn 
-from twitter-auth import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET #credentials
+from twittercreds import * #Gets our Twitter credentials
+
+
+def api_setup():
+
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+
+    return api
+
+
+
 
 
 
