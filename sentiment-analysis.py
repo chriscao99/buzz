069afadc5b2a2de78cbdfe9aeec34b1e369f56db
Chris Cao <chriscao@vt.edu>
2018-04-18 16:21:28 -0400

    Adds most likes functionality

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -22,7 +22,7 @@ def api_setup():
 
 twitterAPI = api_setup()
 
-tweets  = twitterAPI.user_timeline(screen_name="chriscao99", count=10)
+tweets  = twitterAPI.user_timeline(screen_name="realDonaldTrump", count=10)
 
 
 #Prints the last 10 tweets
@@ -48,6 +48,13 @@ nicer_tweets['Retweets'] = nump.array([tweet.retweet_count for tweet in tweets])
 
 display(nicer_tweets.head(10))
 print()
+
+#Includes data on most liked tweet
+print()
+mostLikes = nump.max(nicer_tweets['Likes'])
+print("Most likes on any of the last 10 tweets is: {}".format(mostLikes))
+print()
+
 
 
 
