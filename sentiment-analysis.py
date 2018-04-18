7fd91963eb504af0927149f3138d29c601f42429
Chris Cao <chriscao@vt.edu>
2018-04-18 12:44:51 -0400

    Added twitter authentication information file

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,5 +1,6 @@
 #General imports for analysis
 import tweepy
+import twitter-auth
 import pandas as pnds 
 import numpys as nump 
 
@@ -7,3 +8,10 @@ import numpys as nump
 from IPython.display import display
 import matplotlib.pyplot as plot
 import seaborn as sbn 
+from twitter-auth import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET #credentials
+
+
+
+
+
+
