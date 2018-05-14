731ff7bc7f1fb63f76a932045573df0a7393ad6c
Chris Cao <chriscao@vt.edu>
2018-05-14 19:44:52 -0400

    Made major changes to achieve chart being displayed when user hits submit with accurate data

--- index.js
+++ index.js
@@ -35,10 +35,11 @@ f.submit(function(e){
     contentType: "application/json",
 
     success: function(data){
-      m.addClass('show');
-      m.append('<b> Positive: ' + data.pos + '%, ');
-      m.append('Negative: ' + data.neg + '%, ');
-      m.append('Neutral: ' + data.ntrl + '%</b>');
+      displayGraph(data.pos, data.neg, data.ntrl);
+      // m.addClass('show');
+      // m.append('<b> Positive: ' + data.pos + '%, ');
+      // m.append('Negative: ' + data.neg + '%, ');
+      // m.append('Neutral: ' + data.ntrl + '%</b>');
     }
 
   });
@@ -47,40 +48,52 @@ f.submit(function(e){
   f.addClass('explode');
 
   setTimeout(function(){
+    hideGraph();
     s.val('');
     f.removeClass('explode');
     m.html('&nbsp;').removeClass('show');
     p.html('Click to enter a Twitter handle, Enter to submit');
-  }, 10000);
+  }, 20000);
 });
 
-Highcharts.chart('container', {
-  chart: {
-      type: 'pie',
-      options3d: {
-          enabled: true,
-          alpha: 45
-      }
-  },
-  title: {
-      text: 'Sentiment of user\'s tweets'
-  },
-  // subtitle: {
-  //     text: '3D donut in Highcharts'
-  // },
-  plotOptions: {
-      pie: {
-          innerSize: 100,
-          depth: 45,
-          colors: ['#30FA5E', '#F42C2C', '#959393']
-      }
-  },
-  series: [{
-      name: 'Percentage of Tweets',
-      data: [
-          ['Positive', 60],
-          ['Negative', 30],
-          ['Neutral', 10]
-      ]
-  }]
-});
+function displayGraph(pos, neg, ntrl) {
+  showGraph();
+  Highcharts.chart('container', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45
+        }
+    },
+    title: {
+        text: 'Sentiment of user\'s tweets'
+    },
+    // subtitle: {
+    //     text: '3D donut in Highcharts'
+    // },
+    plotOptions: {
+        pie: {
+            innerSize: 100,
+            depth: 45,
+            colors: ['#30FA5E', '#F42C2C', '#959393']
+        }
+    },
+    series: [{
+        name: 'Percentage of Tweets',
+        data: [
+            ['Positive', pos],
+            ['Negative', neg],
+            ['Neutral', ntrl]
+        ]
+    }]
+  });
+}
+
+function hideGraph() {
+  chart.style.display = "none";
+}
+
+function showGraph() {
+  chart.style.display = "block";
+}
