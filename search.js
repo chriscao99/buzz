43acfe241ad9eb1f1728c49724a58c00bf3fbccf
Chris Cao <chriscao@vt.edu>
2018-06-06 22:15:57 -0400

    added javascript for search page, trying to get title to automatically update now

---
+++ search.js
@@ -0,0 +1,12 @@
+var title = $('h1')
+    result = '';
+
+window.onload = function() {
+    var url = decodeURIComponent(document.location.href);
+        user = url.split('=')[1].split('&');
+    
+    result = user[0];
+    result = result.replace('+', ' ');
+    document.title = result;
+    title.html("Search Results for " + result);
+}
