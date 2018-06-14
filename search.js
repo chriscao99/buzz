b617458b8096ca000e80eed9ec7818f8de9139fd
Chris Cao <chriscao@vt.edu>
2018-06-13 22:51:19 -0400

    Was able to get child nodes, working on actually modifying them

--- search.js
+++ search.js
@@ -9,4 +9,12 @@ window.onload = function() {
     result = result.replace('+', ' ');
     document.title = result;
     title.html("Search Results for " + '"' + result + '"');
+
+    var nodes = document.getElementById('cardcontainer').childNodes;
+    for (var i = 1; i < nodes.length; i++) {
+        if (nodes[i].nodeName.toLowerCase() == "div") {
+            console.log(nodes[i]);
+        }
+    }
+
 }
