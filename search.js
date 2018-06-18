8fe045ceb5921783e5bdcdd19b7318e8ec00c0e9
Chris Cao <chriscao@vt.edu>
2018-06-17 21:25:40 -0400

    Found way to change user fields, just need to modify python script to get the search results

--- search.js
+++ search.js
@@ -10,15 +10,21 @@ window.onload = function() {
     document.title = result;
     title.html("Search Results for " + '"' + result + '"');
 
-    var nodes = document.getElementById('cardcontainer').childNodes;
-    for (var i = 1; i < nodes.length; i++) {
-        if (nodes[i].nodeName.toLowerCase() == "div") {
-            console.log(nodes[i]);
-        }
-    }
+    // var nodes = document.getElementById('cardcontainer').childNodes;
+    // for (var i = 1; i < nodes.length; i++) {
+    //     if (nodes[i].nodeName.toLowerCase() == "div") {
+    //         console.log(nodes[i]);
+    //     }
+    // }
+    var images = document.getElementsByTagName("a");
+    console.log(images);
+    testChangeImages(images);
 
+    var tags = document.getElementsByTagName("span");
+    console.log(tags);
 }
 
-function populateCards(user) {
-    var images = document.getElementById("card").getElementsByTagName();
+function testChangeImages(tag) {
+    console.log(tag[1]);
+    tag[1].innerHTML=('<img src="' + 'https://pbs.twimg.com/profile_images/2319279206/35bwnea39ntayq08qjpb_400x400.png' + '">');
 }
